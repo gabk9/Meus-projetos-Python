529a1249542038e0daf609c6334bf8bb5fa8c711
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2025-07-16 13:51:33 -0300

    Aperfeiçoei o codigo

--- Testes/Media.py
+++ Testes/Media.py
@@ -1,21 +1,9 @@
-n1 = float(input("Me diga um número: "))
-n2 = float(input("Me diga outro número: "))
+numeros = []
 
-media = (n1 + n2) / 2
-div = n1 / n2
-rest = n1 % n2
-soma = n1 + n2
-multi = n1 * n2
-sub = n1 - n2
+while (n := int(input("Digite um número (0 para sair): "))) != 0:
+    numeros.append(n)
 
-print(f"\nA media entre {n1: .2f} e {n2: .2f} é: {media: .2f}")
-print(f"\nA divisão entre {n1: .2f} e {n2: .2f} é: {div: .2f}")
-print(f"\nO resto da divisão entre {n1: .2f} e {n2: .2f} é: {rest: .2f}")
-print(f"\nA soma entre {n1: .2f} e {n2: .2f} é: {soma: .2f}")
-print(f"\nA multiplicação entre {n1: .2f} e {n2: .2f} é: {multi: .2f}")
-print(f"\nA subtração entre {n1: .2f} e {n2: .2f} é: {sub: .2f}")
-
-soma += n1
-print(f"\nO valor da soma de ({n1: .2f} + {n2: .2f}) + {n1: .2f} é: {soma: .2f}")
-sub -= n1
-print(f"O valor da subtração de ({n1: .2f} - {n2: .2f}) - {n1: .2f} é: {sub: .2f}")
+if numeros:
+    print(f"\nA média é: {sum(numeros) / len(numeros):.2f}")
+else:
+    print("\nImpossível calcular a média (nenhum número foi inserido).")
