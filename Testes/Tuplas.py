2a007c320219b34471dd44756e72c9fd3860cd11
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2025-07-18 12:09:02 -0300

    tuplas

---
+++ Testes/Tuplas.py
@@ -0,0 +1,17 @@
+frutas = ("Maçã", "Banana", "Laranja")
+
+qtd = 0
+for i in range(len(frutas)):
+    print("Frutas: ", frutas[i])
+
+print(f"Existem {len(frutas)} de frutas nessa tupla")
+
+
+# frutas = ("Maçã", "Banana", "Laranja")
+
+# qtd = 0
+# for fruta in frutas:
+#     print("Fruta:", fruta)
+#     qtd += 1
+
+# print(f"Existem {qtd} frutas nessa tupla.")
