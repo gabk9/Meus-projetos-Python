157b265f60d4a32e46a9fcb65a1b866feec1f34e
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2025-07-19 11:42:01 -0300

    Criei mais coisas

---
+++ Testes/Sets.py
@@ -0,0 +1,8 @@
+str1 = {"Carlos", "Josiel", "Jandira", "Aline"}
+str2 = {"Aline", "Carlos", "Jaqueline", "Altair"}
+
+print(f"As pessoas presentes nos conjuntos são:\n{str1}\n{str2}")
+
+str3 = str1.union(str2)
+
+print("\nA união dos conjuntos é: ", str3)
