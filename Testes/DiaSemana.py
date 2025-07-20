054ebcb31bb2eb0c07450159d8e484354b11bd87
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2025-07-20 18:53:42 -0300

    Aprimorado

--- Testes/DiaSemana.py
+++ Testes/DiaSemana.py
@@ -1,6 +1,6 @@
 import os
 import msvcrt
-
+    
 def cls():
     os.system('cls')
 
@@ -8,14 +8,15 @@ def pause(msg="Pressione qualquer tecla para continuar..."):
     print(msg)
     msvcrt.getch()
 
-dia = ["\nSaindo...","Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"]
+dia = ["\rSaindo...","Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"]
 
 def main():     
     n = 1
     while n != 0:
         n = int(input("Me diga um número de 1 a 7: "))
-        print(dia[n] if n >= 0 and n < len(dia) else "Opção inválida")
+        print("Dia:", dia[n] if n >= 0 and n < len(dia) else "Opção inválida")
         pause()
         cls()
         
-main()
+if __name__ == "__main__":
+    main()
