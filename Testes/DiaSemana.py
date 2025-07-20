aef19a60c722e6f91480e2a17119509fde454f61
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2025-07-20 18:44:55 -0300

    Aprimorei o código

--- Testes/DiaSemana.py
+++ Testes/DiaSemana.py
@@ -8,23 +8,14 @@ def pause(msg="Pressione qualquer tecla para continuar..."):
     print(msg)
     msvcrt.getch()
 
-dia = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"]
+dia = ["\nSaindo...","Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"]
 
 def main():     
     n = 1
     while n != 0:
+        n = int(input("Me diga um número de 1 a 7: "))
+        print(dia[n] if n >= 0 and n < len(dia) else "Opção inválida")
+        pause()
         cls()
-        n = int(input("Me diga um número de 1 a 7, 0 para sair: "))
-
-        if n < 0 or n > 7:
-            print("Valor inválido!!")
-            pause()
-        elif n == 0:
-            print("Saindo...")
-            break
-        else:
-            print(f"Dia: {dia[n - 1]}")
-            pause()
-            
-if __name__ == "__main__":
-    main()
+        
+main()
