6efd1cde18e5b978dd9c4c3a868dc123d725c6bf
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2025-08-22 15:06:57 -0300

    Mudei um pouco o codigo

--- Casa/Atividade_17.py
+++ Casa/Atividade_17.py
@@ -1,6 +1,11 @@
-LISTA = [5, 2, 9, 1]
-for i in range(len(LISTA)):
-    for j in range(i+1, len(LISTA)):
-        if LISTA[i] > LISTA[j]:
-            LISTA[i], LISTA[j] = LISTA[j], LISTA[i]
-print(LISTA) 
+lista = [2, 5, 4, 3, 1]
+aux = 0
+
+for i in range(len(lista) - 1):
+    for j in range(len(lista) - i - 1):
+        if lista[j] > lista [j + 1]:
+            aux = lista[j]
+            lista[j] = lista[j + 1]
+            lista[j + 1] = aux
+
+print(lista)
