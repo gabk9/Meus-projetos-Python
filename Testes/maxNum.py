a6f5452ef0ceba66eae9bacfee15496199e9f528
Gabriel Oliveira <giane.ga2010@gmail.com>
2025-09-28 13:48:33 -0300

    Benchmark

--- Testes/maxNum.py
+++ Testes/maxNum.py
@@ -5,6 +5,7 @@ FILE: str = "maxNum.txt"
 MAXNUMS: int = 10**8
 MAXGEN: int = 2**63
 #! to generate 10e8 64bits numbers and find the max took 2mins and 13s, 1.85gb file, using hdd, i5-4690, 16gb of ram and using Linux
+#! to generate 10e8 64bits numbers and find the max took 4mins and 36.3s, 1.94gb file, using ssd, i5-4690, 16gb of ram and using Windows
 
 if not os.path.exists(FILE):
     with open(FILE, "w") as f:
